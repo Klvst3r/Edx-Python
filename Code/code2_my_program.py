63459324c4e775f71fc67d6989a0e104fe23a86b
Klvst3r <klvst3r@gmail.com>
2016-01-15 18:39:01 -0600

    Semana 1 finalizada y primeros codigos

---
+++ Code/code2_my_program.py
@@ -0,0 +1,8 @@
+Height_of_rectangle = 20
+Width_of_rectangle = 15
+area = Height_of_rectangle * Width_of_rectangle
+perimeter = (Height_of_rectangle * Width_of_rectangle)* 2
+
+print("area of the rectangle is: ", area)
+print("perimeter of the rectangle is: ", perimeter)
+
